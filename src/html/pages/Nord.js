fe55d6d2d91374c31524e7556a0038786e5a3959
TODSAPORN POOLSILP <56831786+Zixzorash@users.noreply.github.com>
2023-08-16 05:53:06 +0700

    Create Nord.js

---
+++ src/html/pages/Nord.js
@@ -0,0 +1,16 @@
+<div class="nav-wrapper position-relative">
+    <ul class="nav nav-pills rounded nav-fill flex-column flex-md-row">
+        <li class="nav-item">
+            <a class="nav-link mb-sm-3 mb-md-0 active" data-toggle="tab" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link mb-sm-3 mb-md-0" data-toggle="tab" href="#">Profile</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link mb-sm-3 mb-md-0" data-toggle="tab" href="#">Settings</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link mb-sm-3 mb-md-0" data-toggle="tab" href="#">Messages</a>
+        </li>
+    </ul>
+</div>
